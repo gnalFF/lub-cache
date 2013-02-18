3cf6d4ad7aa21ba52549bcdc6369ab68e5cf2959
gnalFF <gnalff@googlemail.com>
2013-02-18 12:51:54 +0100

    added minification support added testconfig for minified files

--- modules/lub-cache.js
+++ modules/lub-cache.js
@@ -1,30 +1,30 @@
 angular.module('lub-cache', ["lub-storage"])
-    .factory("lubCache", function (lubStorage, $log) {
-        return function (name, options) {
-            return {
-                info:function () {
-                    return this.$cache.info();
-                },
-                put:function (key, val, options) {
-                    this.$cache.setItem(key, val, options);
-                },
-                get:function (key) {
-                   return this.$cache.getItem(key);
-                },
-                remove:function (key) {
-                    this.$cache.removeItem(key);
-                },
-                removeAll:function () {
-                    this.$cache.clear();
-                },
-                destroy:function () {
-                    $log.debug("will just invoke removeAll");
-                    this.removeAll();
-                },
-                $cache:lubStorage(name, angular.extend({
-                    ttl:0,
-                    storage:"localStorage"
-                }, options))
-            };
+    .factory("lubCache", ["lubStorage", "$log", function (lubStorage, $log) {
+    return function (name, options) {
+        return {
+            info:function () {
+                return this.$cache.info();
+            },
+            put:function (key, val, options) {
+                this.$cache.setItem(key, val, options);
+            },
+            get:function (key) {
+                return this.$cache.getItem(key);
+            },
+            remove:function (key) {
+                this.$cache.removeItem(key);
+            },
+            removeAll:function () {
+                this.$cache.clear();
+            },
+            destroy:function () {
+                $log.debug("will just invoke removeAll");
+                this.removeAll();
+            },
+            $cache:lubStorage(name, angular.extend({
+                ttl:0,
+                storage:"localStorage"
+            }, options))
         };
-    });
+    };
+}]);
